ec2b71e036c43c7e69c0291efeffc61d721e551d
Daniel Rigberg <daniel.rigberg@gmail.com>
2022-08-17 13:12:56 +0200

    style

--- src/bin/bootstrapDatabase.ts
+++ src/bin/bootstrapDatabase.ts
@@ -1,4 +1,4 @@
-import thingsCtrl from '../controllers/things';
+import things from '../controllers/things';
 import { AppDataSource } from '../database/data-source';
 
 import type Thing from '../database/entities/Thing';
@@ -12,13 +12,13 @@ async function bootstrap(): Promise<Testdata> {
 
   await console.log('Deleting existing data...');
 
-  await thingsCtrl.deleteAll();
+  await things.deleteAll();
 
   console.info('Creating testdata...');
 
   console.info('Inserting a new thing into the database...');
 
-  const thing = await thingsCtrl.create({
+  const thing = await things.create({
     firstName: 'Wendy',
     lastName: 'Corduroy',
     age: 17,
@@ -27,11 +27,11 @@ async function bootstrap(): Promise<Testdata> {
 
   console.info('Loading things from the database...');
 
-  const things = await thingsCtrl.fetchMany();
+  const thingsList = await things.fetchMany();
 
   console.info('Loaded things!', things);
 
-  return { things };
+  return { things: thingsList };
 }
 
 bootstrap()
